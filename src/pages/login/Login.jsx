0fefad6306dbf068dce91d34eb89513785dc3913
Vlad Svezhentseev <paper_plane@ukr.net>
2022-10-10 13:55:02 +0300

    auth fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
 
@@ -28,47 +28,22 @@ function Login() {
          });
    };
 
-   const handleLoginWithGoogle = () => {
-      signInWithPopup(auth, provider)
-         .then((result) => {
-            const user = result.user;
-            dispatch({ type: "LOGIN", payload: user });
-            navigate("/");
-         })
-         .catch((error) => {
-            console.log(error);
-         });
-   };
-
    return (
       <div className="login">
          <form onSubmit={handleLogin}>
             <input
                type="email"
-               placeholder="Email"
+               placeholder="admin@fire.com"
                onChange={(e) => setEmail(e.target.value)}
             />
             <input
                type="password"
-               placeholder="Password"
+               placeholder="123456"
                onChange={(e) => setPassword(e.target.value)}
             />
             <button type="submit">Login</button>
             {error && <span>Wrong email or password!</span>}
          </form>
-         <div onClick={handleLoginWithGoogle}>
-            <div className="google-btn">
-               <div className="google-icon-wrapper">
-                  <img
-                     className="google-icon"
-                     src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                  />
-               </div>
-               <p className="btn-text">
-                  <b>Sign in with google</b>
-               </p>
-            </div>
-         </div>
       </div>
    );
 }
